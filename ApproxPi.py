d88f00de1c0353487bdc3e5a0248a0a567c06174
emmabb426 <ebarnes17@unomaha.edu>
2026-02-09 05:22:23 +0000

    End of Approximate Pi

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name: Emma Barnes
+#Date: 02/08/2026
+#Assignment: Approximate Pi
 import math
 import time
 
@@ -12,15 +12,25 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
-
+  DecPrec = int(input("To what decimal precision do you want me to round Pi? (1-10): "))
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxPi, DecPrec) != round(realPi, DecPrec):
+    print(approxPi)
+    approxPi = approxPi +(sign * 4/denom)
 
+    sign = sign * -1
+    denom = denom +2
+
+  
   end = time.time()
 
   elapsedTime = end - start
-  print(elapsedTime)
+  print("Time elapsed:", elapsedTime)
 
 if __name__ == '__main__':
   main()
